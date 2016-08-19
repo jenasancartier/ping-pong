d325ba1e5b5ce39475e6d2bbd41a5f8d70569472
Jena SanCartier <jenasancartier@gmail.com>
2016-08-18 21:50:33 -0700

    complete first spec:return range of numbers from 1 to the user's chosen number

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
-//back-end logic:
-
+// back-end logic:
+var createPingPongArray = function(number) {
+	var pingPongArray = [];
+  for (var i = 1; i <= number; i++) {
+  	pingPongArray.push(i);
+  }
+  return pingPongArray;
+};
 
 
 //front-end logic:
@@ -7,11 +13,12 @@ $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#userNumber").val());
-    // var result = pingPong(number)
+    var result = createPingPongArray(number);
 
-  $("#result").text(number);
 
-  $("#result").show();
+  $("#result").text(result.join(" "));
+
+
 
   });
 });
